c3c2e2cda1806caf1e2ec114b29f5677019e4cfc
Hanzla <Hanzla@DESKTOP-667DCQV>
2024-05-28 21:22:46 -0700

    Add project files.

---
+++ Rockey/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Rockey.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
